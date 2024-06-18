4c785ce472aec80e0b759480322dd300ae8198ec
ALi4576 <aliasad71734@gmail.com>
2024-06-18 14:01:01 +0200

    chore: removed creds

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ final supabase = Supabase.instance.client;
 
 loadSupabase() async {
   await Supabase.initialize(
-    url: 'https://psagzvmjohwcpcjdzpww.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzYWd6dm1qb2h3Y3BjamR6cHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwMDgyMDEsImV4cCI6MjAzMzU4NDIwMX0.GmsKn0Dvf54GHd0z62FFXlowirq7ZTdFclFVt9y68b0',
+        '',
     // debug: false,
     // authOptions: const FlutterAuthClientOptions(
     //   autoRefreshToken: false,
